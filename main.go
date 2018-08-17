6dea1e44feec439e73de913a60be8d09f5c29eea
mds1455975151 <1455975151@qq.com>
2018-08-17 17:49:17 +0800

    update files

--- main.go
+++ main.go
@@ -16,6 +16,28 @@ package main
 
 import "github.com/mds1455975151/gin-demo/cmd"
 
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server Petstore server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host petstore.swagger.io
+// @BasePath /v2
+
 func main() {
+	// @title Golang Gin API
+	// @version 1.0
+	// @description An example of gin
+	// @termsOfService https://github.com/EDDYCJY/go-gin-example
+
+	// @license.name MIT
+	// @license.url https://github.com/EDDYCJY/go-gin-example/blob/master/LICENSE
 	cmd.Execute()
 }
--- server/server.go
+++ server/server.go
@@ -1,8 +1,34 @@
+// Package classification loveauth API.
+//
+// Auth server for World of Love.
+//
+// Terms Of Service:
+//
+// there are no TOS at this moment, use at your own risk we take no responsibility
+//
+//     Schemes: http, https
+//     BasePath: /api/v1
+//     Version: v1.0.64 -- develop(0efd925)
+//     License: Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0.html
+//     Contact: xy.kong<xy.kong@gmail.com>
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+//
+// swagger:meta
+
 package server
 
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
+
+	"github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
 var logo = `
@@ -11,13 +37,34 @@ var logo = `
     Contact: 1455975151@qq.com <1455975151@qq.com>
 `
 
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server Petstore server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host petstore.swagger.io
+// @BasePath /v2
+
 func Run() {
 	logrus.Info(logo)
 	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	// use ginSwagger middleware to
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
