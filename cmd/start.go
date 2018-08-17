1e8c5216c613b71f5f34ee0f43d54f82cf602b9b
mds1455975151 <1455975151@qq.com>
2018-08-17 15:38:21 +0800

    update fils

---
+++ cmd/start.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/mds1455975151/gin-demo/server"
+)
+
+var startCmd = &cobra.Command{
+	Use:   "start",
+	Short: "Print the version number of gin-demo",
+	Long:  `All software has versions, This is gin-demo's'`,
+	Run: func(cmd *cobra.Command, args []string) {
+		server.Start()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(startCmd)
+}
